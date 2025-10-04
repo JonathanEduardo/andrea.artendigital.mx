773b09d72156890f769a1e0a6eced282b4a682bc
Jonathan Quistiano <jhonquistiano@gmail.com>
2025-10-03 20:28:30 -0600

    Diseño tarjeta

--- src/components/organisms/CardPresentation.jsx
+++ src/components/organisms/CardPresentation.jsx
@@ -3,20 +3,29 @@ import whatsAppIcon from '../../assets/img/icons/whatsapp.png'
 import linkedinIcon from '../../assets/img/icons/linkedin.png'
 import instagramIcon from '../../assets/img/icons/instagram.png'
 
-const CardPresentation = ({ title="Andrea", body="Artista digital", image="/assets/img/carusel/webapp.png", path="/assets/img/carusel/webapp.png" }) => {
+const CardPresentation = ({ title="Andrea", body="Artista digital", image="/assets/img/carusel/webapp.png", path="/assets/img/carusel/webapp.png", qr="" }) => {
     return (
         <div className=' card'>
-            <h1 className="title">{title}</h1>
-            <p className="text-neutral-700 mt-2 font-bold">{body}</p>
-            <img src={image} alt={title} className='w-1/2 rounded-full' />
-            <a href={path} className="mt-4 inline-block px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors duration-300">Ver más</a>
-            <div className="mt-4">
-                <p className="text-neutral-700 font-bold">Contacto</p>
+            <div className="flex flex-col items-center">
+                <h1 className="title">{title}</h1>
+                <p className="body">{body}</p>
+                <img src={image} alt={title} className='photo' />
             </div>
+            <div>
+                <a href={path} className="showMore">Ver más</a>
+            </div>
+            {/* <div className="mt-4">
+                <p className="text-neutral-700 font-bold">Contacto</p>
+                <div className="qr">
+                    <img src={qr} alt="" />
+                </div>
+            </div> */}
+     
+        <hr />  
            <div className='flex flex-row justify-center gap-10 w-full '>
-                <a href="https://wa.me/5219991234567" target="_blank" rel="noopener noreferrer"><img src={whatsAppIcon} alt="WhatsApp" className="w-6 h-6" /></a>
-                <a href="https://www.linkedin.com/in/andrea-leticia-venegas-cepeda/" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} alt="LinkedIn" className="w-6 h-6" /></a>
-                <a href="https://www.instagram.com/andrea_leticia_venegas_cepeda/" target="_blank" rel="noopener noreferrer"><img src={instagramIcon} alt="Instagram" className="w-6 h-6" /></a>
+                <a href="https://wa.me/5219991234567" target="_blank" rel="noopener noreferrer"><img src={whatsAppIcon} alt="WhatsApp" className="icon" /></a>
+                <a href="https://www.linkedin.com/in/andrea-leticia-venegas-cepeda/" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} alt="LinkedIn" className="icon" /></a>
+                <a href="https://www.instagram.com/andrea_leticia_venegas_cepeda/" target="_blank" rel="noopener noreferrer"><img src={instagramIcon} alt="Instagram" className="icon" /></a>
            </div>
         </div>
     );
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,17 +4,17 @@ import CardPresentation from '../../components/organisms/CardPresentation';
 
 const HomePage = () => {
     return (
-        <div className="flex flex-col md:flex-row  p-4 md:p-6 lg:p-8">
-            <div className="md:w-1/3 w-full   ">
+        <div className="flex flex-col md:flex-row  p-4 md:p-6 lg:p-8 ">
+            <div className="md:w-1/3 w-full  animate-fade-down animate-once animate-duration-2000  ">
                 <CardPresentation 
                 title="Andrea Leticia Venegas Cepeda"
                 body="Ceo de Arten Digital"
                 image="/assets/img/profile/andrea.png"
                 path="/assets/img/profile/andrea.png"
-                
+                qr="/assets/img/profile/qrAndrea.png"
                 />
             </div>
-            <div className="md:w-2/3 w-full">
+            <div className="md:w-2/3 w-full animate-fade-up animate-once animate-duration-2000 ">
                 <HeroCarousel />
             </div>
         </div>
